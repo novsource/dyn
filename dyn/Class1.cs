55159b62e82844be4c70962d8f9ddcfbd62a9547
Evstafev <evstafev.nik@yandex.ru>
2022-03-30 05:44:23 +0800

    yy

---
+++ dyn/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dyn
+{
+    public class CalculatorExtended
+    {
+        public static string Sin(double x)
+        {
+            return string.Format("{0}", Math.Sin(x));
+        }
+
+        public static string Cos(double x)
+        {
+            return string.Format("{0}", Math.Cos(x));
+        }
+
+        public static string Tan(double x)
+        {
+            return string.Format("{0}", Math.Tan(x));
+        }
+
+        public static string Ctg(double x)
+        {
+            return string.Format("{0}", 1/Math.Tan(x));
+        }
+    }
+}
